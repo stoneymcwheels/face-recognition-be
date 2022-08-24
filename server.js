97a950e729f7852db76ec59d61eb9f3f74ee349a
stoneymcwheels <stoneymcwheels@protonmail.com>
2022-08-24 17:54:05 -0400

    proxy and port

--- server.js
+++ server.js
@@ -13,16 +13,15 @@ const db = knex({
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : 'cf420',
+    password : '',
     database : 'smart-brain'
   }
 });
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000/", credentials: true }))
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
-
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
@@ -30,6 +29,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT});
 })
