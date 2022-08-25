a088dc86ba57756c1be97caf77b83f5329c70752
stoneymcwheels <stoneymcwheels@protonmail.com>
2022-08-25 15:54:13 -0400

    cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const cors = require('cors');
 
 const db = knex({
   client: 'pg',
@@ -15,11 +16,12 @@ const db = knex({
 });
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt,) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
